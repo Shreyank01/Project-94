2f43fee54630e8a2ce266581cd5a12b0576787f1
Shreyank01 <87267295+Shreyank01@users.noreply.github.com>
2021-12-14 18:46:09 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyANUEujwtyrVNUUahaP2nHutgJiWcPFpAM",
+    authDomain: "kwitter-project-update-50372.firebaseapp.com",
+    databaseURL: "https://kwitter-project-update-50372-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-update-50372",
+    storageBucket: "kwitter-project-update-50372.appspot.com",
+    messagingSenderId: "44819131699",
+    appId: "1:44819131699:web:28969da8f70e2a8a84e246",
+    measurementId: "G-3MCGCFG9VK"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
